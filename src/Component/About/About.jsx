4e45713ca8cf0e4c85e84240dfdc1c5c39a51dbc
Himja Parekh <himjaparekh2000@gmail.com>
2024-02-22 20:20:20 -0500

    about section updated

--- src/Component/About/About.jsx
+++ src/Component/About/About.jsx
@@ -1,51 +1,76 @@
 import "./About.scss";
-import itWork from "../../Assets/Image/it-work.jpg"
+import itWork from "../../Assets/Image/it-work.jpg";
 function About() {
   return (
-    <div>
-      <h1>
+    <div className="about__main">
+      <h1 className="about__title">
         IT That <bold>Works</bold> For You
       </h1>
-          <div>
-              <img src={itWork} alt="working technicians" />
+      <div className="about__img-container">
+        <img src={itWork} alt="working technicians" className="about__img" />
+        <div className="about__img-border"></div>
+      </div>
+      <div className="about-content__container">
+        <p className="about-content__para">
+          Started in 2024,{" "}
+          <span
+            style={{ color: "#0251bf", fontWeight: "bold", fontSize: "26px" }}
+          >
+            Tech Haven{" "}
+          </span>
+          is your Trusted IT Support Partner for Small Businesses and Homes. At
+          Tech Haven, we understand the critical importance of technology in
+          today's fast-paced world. Our mission is to provide comprehensive and
+          reliable IT support services to small businesses and residences,
+          enabling our clients to focus on what matters most - growing their
+          business or enjoying their home life.
+        </p>
       </div>
       <div>
-        <p>
-          Started in 2024, <bold>Tech Haven</bold>is your Trusted IT Support
-          Partner for Small Businesses and Homes. At Tech Haven, we understand
-          the critical importance of technology in today's fast-paced world. Our
-          mission is to provide comprehensive and reliable IT support services
-          to small businesses and residences, enabling our clients to focus on
-          what matters most - growing their business or enjoying their home
-          life.
-        </p>
-
-        <h2>What Makes Us Stand Out?</h2>
-        <ul>
+        <h2 className="about-content__subtitle">What Makes Us Stand Out?</h2>
+        <ul className="about-content__list">
           <li>
-            Customer Focus: Our clients are our top priority. We go above and
-            beyond to provide exceptional service and build long-term
-            relationships.
+            <span
+              style={{ color: "#0251bf", fontWeight: "bold", fontSize: "22px" }}
+            >
+              Customer Focus
+            </span>
+            : Our clients are our top priority.
           </li>
           <li>
-            Innovation: We are constantly seeking new and better ways to serve
-            our clients. We are at the forefront of technological advancements
-            and strive for continuous improvement.
+            <span
+              style={{ color: "#0251bf", fontWeight: "bold", fontSize: "22px" }}
+            >
+              Innovation
+            </span>
+            : We are constantly seeking new and better ways to serve our
+            clients.
           </li>
           <li>
-            Transparency: We are committed to open and honest communication. You
-            can trust us to keep you informed every step of the way and to
-            provide clear, accurate information.
+            <span
+              style={{ color: "#0251bf", fontWeight: "bold", fontSize: "22px" }}
+            >
+              Transparency
+            </span>
+            : We are committed to open and honest communication.
           </li>
           <li>
-            Flexibility: We understand that every client is unique, and we are
-            flexible in our approach. We will work with you to find solutions
-            that meet your specific needs and requirements.
+            <span
+              style={{ color: "#0251bf", fontWeight: "bold", fontSize: "22px" }}
+            >
+              Flexibility
+            </span>
+            :We will work with you to find solutions that meet your specific
+            needs and requirements.
           </li>
           <li>
-            Responsiveness: We understand that technology issues can be time
-            sensitive. We are committed to responding quickly to your needs and
-            providing timely solutions.
+            <span
+              style={{ color: "#0251bf", fontWeight: "bold", fontSize: "22px" }}
+            >
+              Responsiveness
+            </span>
+            : We are committed to responding quickly to your needs and providing
+            timely solutions.
           </li>
         </ul>
       </div>
